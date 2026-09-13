2c59d6b418b0a2761b4c138a23d4083a2b905e19
Dhanalaxmi Devunoori <dhanadevunoori@gmail.com>
2026-09-13 20:23:51 +0530

    Enable TLS for cloud database connections

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,6 +6,14 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+
+  ssl:
+    process.env.DB_SSL === "true"
+      ? {
+          minVersion: "TLSv1.2",
+        }
+      : undefined,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
